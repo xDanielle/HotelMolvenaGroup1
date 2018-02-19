755589c42748081f114acf9d20a8042c45a20cad
Daniëlle Duijst <dmsduijst@gmail.com>
2018-02-19 16:52:47 +0100

    Initial commit

---
+++ src/main/java/com/capgemini/HotelMolvenaGr1/HotelMolvenaGr1Application.java
@@ -0,0 +1,12 @@
+package com.capgemini.HotelMolvenaGr1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelMolvenaGr1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelMolvenaGr1Application.class, args);
+	}
+}
