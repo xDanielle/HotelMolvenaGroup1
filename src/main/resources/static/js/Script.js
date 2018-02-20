b29166c92782847775f63c927e656c86f2c08cee
Daniëlle Duijst <dmsduijst@gmail.com>
2018-02-20 17:05:18 +0100

    html files

--- src/main/resources/static/js/Script.js
+++ src/main/resources/static/js/Script.js
@@ -1,11 +1,8 @@
 // Javascript bestand
 // alert("Testje"); Mag eigenlijk niet meer van Robbert
 
-// testje met menuitems
-//$(document).ready(function() {
-//    $(".container").load("index.html");
 //
-//});
+
 
 
 
