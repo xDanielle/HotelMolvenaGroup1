abc464280131c0e68d2ce9edd8227140c4615e8d
Daniëlle Duijst <dmsduijst@gmail.com>
2018-02-20 14:40:35 +0100

    Index veranderd

--- src/main/resources/static/js/Script.js
+++ src/main/resources/static/js/Script.js
@@ -1,2 +1,13 @@
 // Javascript bestand
 // alert("Testje"); Mag eigenlijk niet meer van Robbert
+
+
+$(document).ready(function() {
+    $(".container").load("index.html");
+});
+
+$("ul.navbar-nav li").each(function() {
+    $(this).on("click", function{
+        $(".container").load(($this).attr("rooms")+".html");
+    });
+});
