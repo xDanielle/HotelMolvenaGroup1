ff5326ea19ac307394e7b08d410a25783a8bf1b5
suuzels <suzannevdmeer@gmail.com>
2018-02-20 14:00:46 +0100

    made an Enum of roomtypes and deleted different classes for rooms.

---
+++ src/main/java/com/capgemini/HotelMolvenaGr1/Model/ERoomType.java
@@ -0,0 +1,5 @@
+package com.capgemini.HotelMolvenaGr1.Model;
+
+public enum ERoomType {
+    STANDARD, LUXURY, HONEYMOON_SUITE
+}
--- src/main/java/com/capgemini/HotelMolvenaGr1/Room.java
+++ src/main/java/com/capgemini/HotelMolvenaGr1/Room.java
@@ -1,21 +1,42 @@
 package com.capgemini.HotelMolvenaGr1;
 
+import com.capgemini.HotelMolvenaGr1.Model.ERoomType;
+import java.util.Date;
+
 public abstract class Room {
 
-    public Room(){}
+    // Variabelen
+    private ERoomType roomType;
 
-    private boolean isOccupied;
+    private Date dateAvailable;
 
     private int roomID;
+
+    private int roomSize;
 
     private double defaultPrice;
 
     private int numberOfBeds;
 
-    public abstract void calculateEventPrice();
+    // Constructor
+    public Room(){}
 
+    public Room(ERoomType roomType, Date dateAvailable, int roomID, double defaultPrice) {
+        this.roomType = roomType;
+        this.dateAvailable = dateAvailable;
+        this.roomID = roomID;
+        this.defaultPrice = defaultPrice;
+    }
+
+    // Methodess
     public void alterRoom(){
 
+    }
+
+    public double calculateEventPrice() {
+        double eventPrice = defaultPrice * 2;
+        System.out.println("The new price of the room is: " + eventPrice + " euro.");
+        return eventPrice;
     }
 
 }
