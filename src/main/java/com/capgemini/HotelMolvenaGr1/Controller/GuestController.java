2f6d20f9462aea08063c1834990b3c57cd094d3a
SashaVollebregt <SashaVollebregt@users.noreply.github.com>
2018-02-21 11:16:38 +0100

    add
    guestcontroller

---
+++ src/main/java/com/capgemini/HotelMolvenaGr1/Controller/GuestController.java
@@ -0,0 +1,31 @@
+package com.capgemini.HotelMolvenaGr1.Controller;
+
+
+import com.capgemini.HotelMolvenaGr1.Model.Guest;
+import com.capgemini.HotelMolvenaGr1.Model.GuestRegister;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+public class GuestController {
+    @RequestMapping("/api/found")
+    public String searchGuest(){
+        GuestRegister guest = new GuestRegister();
+        boolean result = guest.searchGuest("Sasha");
+        if(result) {
+            return "found";
+        }else{
+            return "not found";
+        }
+    }
+
+    @RequestMapping("/api/guest")
+    public List<Guest> removeGuest() {
+        GuestRegister removeGuest = new GuestRegister();
+        removeGuest.removeGuest("Sasha");
+        return removeGuest.getGuestList();
+    }
+
+
+}
