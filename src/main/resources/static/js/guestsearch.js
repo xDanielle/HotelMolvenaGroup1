070345030a9b8bf280f3715ba07dd850629b3479
flogtenb <36152895+flogtenb@users.noreply.github.com>
2018-02-23 13:58:51 +0100

    ch

---
+++ src/main/resources/static/js/guestsearch.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $.ajax({
+        url:"http://localhost:8080/api/guest/index",
+        type:"get",
+        success : function(data){
+            console.log(data);
+            var guestList = '';
+
+            $.each(data, function(index, value){
+                        var columnRow = "<tr><td><input type='checkbox'</td><td>" + value.firstName + "</td><td>" +
+                        value.lastName + "<td>" + value.address + "</td><td>" + value.country +
+                        "</td><td>" + value.town + "</td><td>" + value.postalCode + "</td><td>" +
+                         value.telephoneNumber + "</td><td>" + value.emailAddress + "</td></tr>";
+                        guestList+=columnRow;
+                    });
+             $("#guest").append(guestList);
+        }
+    });
+});
