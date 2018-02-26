d76d1101a8a6c474bf3b9a60d7623752b12c81b0
suuzels <suzannevdmeer@gmail.com>
2018-02-26 13:38:36 +0100

    added RequestMappings

--- src/main/java/com/capgemini/HotelMolvenaGr1/controller/BookingController.java
+++ src/main/java/com/capgemini/HotelMolvenaGr1/controller/BookingController.java
@@ -1,6 +1,9 @@
 package com.capgemini.HotelMolvenaGr1.controller;
 
+import com.capgemini.HotelMolvenaGr1.booking.Booking;
 import com.capgemini.HotelMolvenaGr1.repository.BookingRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +13,20 @@ public class BookingController {
 
     // RequestMappings
 
+    @RequestMapping(value = "api/bookings/get", method = RequestMethod.GET)
+    public Iterable<Booking> index(){
+        return bookingRepository.getBookings();
+    }
 
+    @RequestMapping(value = "api/bookings/save", method = RequestMethod.POST)
+    public void save(Booking bookingToSave){
+        bookingRepository.save(bookingToSave);
+    }
+
+    @RequestMapping(value = "api/bookings/delete", method = RequestMethod.DELETE)
+    public void deleteBooking(){
+        bookingRepository.deleteBooking();
+    }
+    
 
 }
--- src/main/java/com/capgemini/HotelMolvenaGr1/repository/BookingRepository.java
+++ src/main/java/com/capgemini/HotelMolvenaGr1/repository/BookingRepository.java
@@ -54,5 +54,4 @@ public class BookingRepository {
         this.bookings = bookings;
     }
 
-
 }
